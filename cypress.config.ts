d6e2a0eb9427a31f8cc88f18cf87cd8579175753
Luiz Teles <luizteles934@gmail.com>
2024-06-05 14:14:14 -0300

    Remover comentários desnecessários

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   chromeWebSecurity: false,
   e2e: {
-    // Base URL da aplicação
     baseUrl: 'http://localhost:5173',
-
-    // Impede de limpar o estado da tela após cada it
     testIsolation: false,
   },
 });
